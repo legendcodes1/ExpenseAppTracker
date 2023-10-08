67241e558e567a4133b0d81da804bbd4108e276a
fospwd <fospwd@umsystem.edu>
2023-10-08 15:27:55 -0500

    second commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChar';
 
 
 const Expenses = (props) => {
@@ -29,23 +29,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === choosenDate;
   });
 
-  let expensesContent = <p> No expenses found</p> ;
-  if(filteredExpenses.length > 0){
-    expensesContent =  filteredExpenses.map((expense) => (
-      <ExpenseItem  
-   key={expense.id}
-   title={expense.title}
-       amount={expense.amount}
-       date={expense.date}
-       />
-       ))
-  }
   return (
     <div>
     <Card className="expenses">
     <ExpensesFilter selected={choosenDate} onChangeFilter={filterChangeHandler}/>
-   
-      {expensesContent}   
+   <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses}/>
        </Card>
     </div>
   );
